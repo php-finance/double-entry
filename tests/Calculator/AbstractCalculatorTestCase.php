0a48e8d3dee94bb47306d9f790872d499321b9dd
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-07-05 14:31:10 +0300

    test

--- tests/Calculator/AbstractCalculatorTestCase.php
+++ tests/Calculator/AbstractCalculatorTestCase.php
@@ -34,7 +34,7 @@ abstract class AbstractCalculatorTestCase extends TestCase
         $incomesAccount = self::createAccount('incomes');
         $expensesAccount = self::createAccount('expenses');
         $walletAccount = self::createAccount('wallet');
-        $data['base'] = [
+        $data['without-filter'] = [
             (new MoneyBag())->add(Money::of(275, 'USD')), // 100 + 150 + 25
             (new MoneyBag())->add(Money::of(30, 'USD')), // 30
             (new MoneyBag())->add(Money::of(245, 'USD')), // 100 + 150 - 30 + 25
@@ -74,7 +74,7 @@ abstract class AbstractCalculatorTestCase extends TestCase
         $incomesAccount = self::createAccount('incomes');
         $expensesAccount = self::createAccount('expenses');
         $walletAccount = self::createAccount('wallet');
-        $data['base'] = [
+        $data['filter-by-one-dimension'] = [
             (new MoneyBag())->add(Money::of(100, 'USD')),
             (new MoneyBag())->add(Money::of(30, 'USD')),
             (new MoneyBag())->add(Money::of(70, 'USD')),
@@ -98,6 +98,60 @@ abstract class AbstractCalculatorTestCase extends TestCase
             ],
             $walletAccount,
             new Filter(dimensions: ['contractor' => 'John']),
+        ];
+
+        /**
+         * Calculate with filter by two dimension
+         *
+         * Dr wallet (John, High) Cr incomes 100 USD
+         * Dr wallet (John, Normal) Cr incomes 90 USD
+         * Dr wallet (John, High) Cr incomes 5 USD
+         * Dr expenses Cr wallet (John, High) 30 USD
+         * Dr wallet (Mike, High) Cr incomes 150 USD
+         * Dr expenses Cr wallet (John, Normal) 10 USD
+         */
+        $incomesAccount = self::createAccount('incomes');
+        $expensesAccount = self::createAccount('expenses');
+        $walletAccount = self::createAccount('wallet');
+        $data['filter-by-two-dimensions'] = [
+            (new MoneyBag())->add(Money::of(105, 'USD')), // 100 + 5
+            (new MoneyBag())->add(Money::of(30, 'USD')),
+            (new MoneyBag())->add(Money::of(75, 'USD')),
+            [$incomesAccount, $expensesAccount, $walletAccount],
+            [
+                self::createPosting(
+                    new EntryData($walletAccount, ['contractor' => 'John', 'priority' => 'High']),
+                    new EntryData($incomesAccount),
+                    amount: Money::of(100, 'USD'),
+                ),
+                self::createPosting(
+                    new EntryData($walletAccount, ['contractor' => 'John', 'priority' => 'Normal']),
+                    new EntryData($incomesAccount),
+                    amount: Money::of(90, 'USD'),
+                ),
+                self::createPosting(
+                    new EntryData($walletAccount, ['contractor' => 'John', 'priority' => 'High']),
+                    new EntryData($incomesAccount),
+                    amount: Money::of(5, 'USD'),
+                ),
+                self::createPosting(
+                    new EntryData($expensesAccount),
+                    new EntryData($walletAccount, ['contractor' => 'John', 'priority' => 'High']),
+                    amount: Money::of(30, 'USD'),
+                ),
+                self::createPosting(
+                    new EntryData($walletAccount, ['contractor' => 'Mike', 'priority' => 'High']),
+                    new EntryData($incomesAccount),
+                    amount: Money::of(150, 'USD'),
+                ),
+                self::createPosting(
+                    new EntryData($expensesAccount),
+                    new EntryData($walletAccount, ['contractor' => 'John', 'priority' => 'Normal']),
+                    amount: Money::of(10, 'USD'),
+                ),
+            ],
+            $walletAccount,
+            new Filter(dimensions: ['contractor' => 'John', 'priority' => 'High']),
         ];
 
         return $data;
